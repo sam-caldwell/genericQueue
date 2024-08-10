0fd192e91d130c9729cfebabce221a37ba736348
Sam Caldwell <github@samcaldwell.net>
2024-08-10 15:44:56 -0500

    pass compareFunc through to Queue object

--- NewGenericQueue.go
+++ NewGenericQueue.go
@@ -1,11 +1,12 @@
 package GenericQueue
 
 // NewGenericQueue - Generate and configure the initial state for a GenericQueue.Queue object
-func NewGenericQueue[T any]() *Queue[T] {
+func NewGenericQueue[T any](compareFunc func(T, T) int) *Queue[T] {
 	queue := Queue[T]{
-		head:  nil,
-		tail:  nil,
-		count: 0,
+		head:        nil,
+		tail:        nil,
+		count:       0,
+		compareFunc: compareFunc,
 	}
 	return &queue
 }
