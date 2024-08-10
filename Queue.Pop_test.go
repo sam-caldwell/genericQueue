97abdd00b813721a3e04c9e92b0de0cdc1360575
Sam Caldwell <github@samcaldwell.net>
2024-08-10 14:52:02 -0500

    add count test to Pop() test

--- Queue.Pop_test.go
+++ Queue.Pop_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestQueue_Pop(t *testing.T) {
 	t.Run("pop static setup", func(t *testing.T) {
 		queue := Queue[uint]{
-			count: 1,
+			count: 3,
 			head: &(QueueNode[uint]{
 				data: 1,
 				nextPtr: &(QueueNode[uint]{
@@ -20,17 +20,32 @@ func TestQueue_Pop(t *testing.T) {
 		if queue.head.data != 1 {
 			t.Fatal("setup 1 is wrong")
 		}
+		if queue.count != 3 {
+			t.Fatal("count is wrong")
+		}
 		if value := queue.Pop(); value != 1 {
 			t.Fatal("value expect 1")
+		}
+		if queue.count != 2 {
+			t.Fatal("count is wrong")
 		}
 		if value := queue.Pop(); value != 2 {
 			t.Fatal("value expect 2")
 		}
+		if queue.count != 1 {
+			t.Fatal("count is wrong")
+		}
 		if value := queue.Pop(); value != 3 {
 			t.Fatal("value expect 3")
 		}
+		if queue.count != 0 {
+			t.Fatal("count is wrong")
+		}
 		if value := queue.Pop(); value != 0 {
 			t.Fatalf("value expect 0 got: %d", value)
+		}
+		if queue.count != 0 {
+			t.Fatal("count is wrong")
 		}
 	})
 }
