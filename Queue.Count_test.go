5df1679f2611c787bcfe0d7df40c775d816171cb
Sam Caldwell <github@samcaldwell.net>
2024-08-10 14:49:14 -0500

    add Count() and test

---
+++ Queue.Count_test.go
@@ -0,0 +1,17 @@
+package GenericQueue
+
+import "testing"
+
+func TestQueue_Count(t *testing.T) {
+	t.Run("Test values 0-100", func(t *testing.T) {
+		for i := uint(0); i < 100; i++ {
+			queue := Queue[int]{
+				count: i,
+			}
+			if queue.count != i {
+				t.Fatalf("expected count (%d) not found. Got %d", i, queue.count)
+			}
+		}
+	})
+
+}
