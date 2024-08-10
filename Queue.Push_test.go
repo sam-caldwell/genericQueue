55e9b289c5d8ebed6e18b96c870bcc41fa51609b
Sam Caldwell <github@samcaldwell.net>
2024-08-10 14:54:58 -0500

    add value check to Queue.Push() test

--- Queue.Push_test.go
+++ Queue.Push_test.go
@@ -8,9 +8,21 @@ func TestQueue_Push(t *testing.T) {
 		if queue.count != 0 {
 			t.Fatalf("expect count 0")
 		}
-		queue.Push(1)
+		if queue.head != nil {
+			t.Fatalf("expect head nil")
+		}
+		if queue.tail != nil {
+			t.Fatalf("expect tail nil")
+		}
+		queue.Push(1337)
 		if queue.count != 1 {
 			t.Fatalf("expect count 1")
+		}
+		if queue.head.data != 1337 {
+			t.Fatalf("expect head.data 1337")
+		}
+		if queue.tail.data != 1337 {
+			t.Fatalf("expect tail.data 1337")
 		}
 	})
 	t.Run("push ten", func(t *testing.T) {
@@ -20,9 +32,18 @@ func TestQueue_Push(t *testing.T) {
 				t.Fatalf("expect count %d", i)
 			}
 			queue.Push(i)
+			if queue.head.data != 0 {
+				t.Fatalf("expect head.data 0 (unchanged)")
+			}
+			if queue.tail.data != i {
+				t.Fatalf("expect tail.data %d (unchanged)", i)
+			}
 		}
 		if queue.count != 10 {
 			t.Fatalf("expect count 10")
+		}
+		if queue.tail.data != 9 {
+			t.Fatalf("expect tail.data 9 (unchanged)")
 		}
 	})
 }
