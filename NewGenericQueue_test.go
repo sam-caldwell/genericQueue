3e7b2edfe93497607d38d4d448c869ea7d35132b
Sam Caldwell <github@samcaldwell.net>
2024-08-10 15:49:18 -0500

    fix compareFunc for NewGenericQueue()

--- NewGenericQueue_test.go
+++ NewGenericQueue_test.go
@@ -3,16 +3,46 @@ package GenericQueue
 import "testing"
 
 func TestNewGenericQueue(t *testing.T) {
-	queue := NewGenericQueue[int]()
-	queue.lock.Lock()
-	queue.lock.Unlock()
-	if queue.head != nil {
-		t.Fail()
-	}
-	if queue.tail != nil {
-		t.Fail()
-	}
-	if queue.count != 0 {
-		t.Fail()
-	}
+	t.Run("basic test", func(t *testing.T) {
+		queue := NewGenericQueue[int](nil)
+		queue.lock.Lock()
+		queue.lock.Unlock()
+		if queue.head != nil {
+			t.Fail()
+		}
+		if queue.tail != nil {
+			t.Fail()
+		}
+		if queue.count != 0 {
+			t.Fail()
+		}
+		if queue.compareFunc != nil {
+			t.Fail()
+		}
+	})
+	t.Run("test with compareFunc", func(t *testing.T) {
+		queue := NewGenericQueue[int](func(a, b int) int {
+			if a == b {
+				return EqualTo
+			}
+			if a < b {
+				return LessThan
+			}
+			return GreaterThan
+		})
+		queue.lock.Lock()
+		queue.lock.Unlock()
+		if queue.head != nil {
+			t.Fail()
+		}
+		if queue.tail != nil {
+			t.Fail()
+		}
+		if queue.count != 0 {
+			t.Fail()
+		}
+		if queue.compareFunc != nil {
+			t.Fail()
+		}
+	})
 }
